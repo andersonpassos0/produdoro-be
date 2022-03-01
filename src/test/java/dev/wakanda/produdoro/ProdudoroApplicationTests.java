47d1c88c696833d972a92cd3022d14cf40c32eaf
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-02-28 23:29:00 -0300

    git commit

---
+++ src/test/java/dev/wakanda/produdoro/ProdudoroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakanda.produdoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdudoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
